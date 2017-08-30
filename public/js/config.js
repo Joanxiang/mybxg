b94920cae2f3ac04b468d9a6a98fe7d794517a3b
zhangsan <abc@163.com>
2017-08-30 20:25:00 +0800

    模板引擎重构

--- public/js/config.js
+++ public/js/config.js
@@ -4,6 +4,7 @@
         paths:{//定义依赖文件的别名
           jquery:'jquery/jquery',
           cookie:'jquery-cookie/jquery.cookie',
+          template:'artTemplate/template-web',
           common:'../js/common',//退出
           login:'../js/login'//登录
         }
